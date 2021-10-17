b08475dda4bf8b989dcf0c04bc7a3a2393fdc52a
Annomaliya <zezeron1994@gmail.com>
2021-10-17 19:26:15 +0300

    try new feature

---
+++ PavelQA/src/QADleE.java
@@ -0,0 +1,21 @@
+public class QADleE {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b = 10;
+        a=b;
+
+        System.out.print((a==b)+" ");
+        System.out.println(a != b);
+        System.out.println(!(false));
+        System.out.println();
+
+        System.out.println(a>b || a<b);
+        System.out.println(a>b && a<b);
+
+        int c = 144;
+        System.out.println(Math.sqrt(c));
+
+    }
+}
