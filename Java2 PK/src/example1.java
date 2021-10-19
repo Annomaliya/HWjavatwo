05f6bc8e594edcb8b6c4245994bed1a7d1cae763
Annomaliya <zezeron1994@gmail.com>
2021-10-19 19:39:42 +0300

    Homework

---
+++ Java2 PK/src/example1.java
@@ -0,0 +1,12 @@
+public class example1 {
+        public static void main(String[] args) {
+            int n;
+            n = 14;
+            if (n%2 == 0) {
+                System.out.println(n + " - четное ");
+            } else {
+                System.out.println(n + " -  не четное ");
+            }
+        }
+    }
+
