2d813d6df0166f58bb035f6f3723fcb515da666e
Annomaliya <zezeron1994@gmail.com>
2021-10-22 22:40:36 +0300

    my mass

---
+++ HWjavatwo/src/Massiv.java
@@ -0,0 +1,20 @@
+public class Massiv {
+    public static void main(String[] args) {
+        int [][] nums = new int[5][8];
+        for(int i =0;i<nums.length;i++){
+            for(int h=0;h<nums[i].length;h++){
+                nums[i][h]=(int)((Math.random()*199)-99);
+                System.out.print(nums[i][h]+" ");
+            }
+            System.out.println();
+        }
+        int min=nums[0][0];
+        for(int i =0;i<nums.length;i++){
+            for(int h=0;h<nums[i].length;h++){
+                if(nums[i][h]>min)min=nums[i][h];
+            }
+        }
+        System.out.println(" ");
+        System.out.println("Максимальное значение в масиве = "+min);
+    }
+}
